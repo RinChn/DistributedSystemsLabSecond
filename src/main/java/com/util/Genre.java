b28f087474c39295ae2b7e3aeb5aff089faab196
RinChn <morik.arina@yandex.ru>
2025-02-12 02:50:24 +0300

    Adding enum for genres

---
+++ src/main/java/com/util/Genre.java
@@ -0,0 +1,21 @@
+package com.util;
+
+public enum Genre {
+    ACTION,
+    ADVENTURE,
+    COMEDY,
+    DRAMA,
+    HORROR,
+    THRILLER,
+    SCIENCE_FICTION,
+    FANTASY,
+    ROMANCE,
+    MYSTERY,
+    CRIME,
+    ANIMATION,
+    DOCUMENTARY,
+    MUSICAL,
+    HISTORICAL,
+    BIOGRAPHY,
+    SPORT
+}
