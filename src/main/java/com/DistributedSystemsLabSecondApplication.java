ba46d624e910f83ee0f0c5315b6a1587b12bf47e
RinChn <morik.arina@yandex.ru>
2025-02-12 02:49:32 +0300

    Initial files

---
+++ src/main/java/com/DistributedSystemsLabSecondApplication.java
@@ -0,0 +1,13 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DistributedSystemsLabSecondApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DistributedSystemsLabSecondApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/distributedsystemslabsecond/DistributedSystemsLabSecondApplicationTests.java
@@ -0,0 +1,13 @@
+package com.distributedsystemslabsecond;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DistributedSystemsLabSecondApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
