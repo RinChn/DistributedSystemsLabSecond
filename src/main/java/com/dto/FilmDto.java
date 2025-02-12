ae188deff16199c86c1d3abca19ae9fbe8f34880
RinChn <morik.arina@yandex.ru>
2025-02-13 02:39:11 +0300

    Fix camel case for director's name in filmDto

--- src/main/java/com/dto/FilmDto.java
+++ src/main/java/com/dto/FilmDto.java
@@ -14,7 +14,7 @@ public class FilmDto {
     @NotBlank(message = "The title cannot be empty")
     private String title;
     @NotBlank(message = "The director's name cannot be empty")
-    private String director_name;
+    private String directorName;
     @Positive(message = "The release year cannot be a zero or a negative number.")
     private Integer yearReleased;
     @Positive(message = "The duration cannot be zero or a negative number.")
