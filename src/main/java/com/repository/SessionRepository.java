4c5087f7318467449d929338899c590e8bb33124
RinChn <morik.arina@yandex.ru>
2025-02-13 02:43:04 +0300

    Adding method for get session by date, time and hall number from rep

--- src/main/java/com/repository/SessionRepository.java
+++ src/main/java/com/repository/SessionRepository.java
@@ -2,10 +2,17 @@ package com.repository;
 
 import com.entity.Session;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.sql.Timestamp;
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface SessionRepository extends JpaRepository<Session, UUID> {
+    @Query("SELECT s FROM Session s WHERE s.cinemaHallNumber = :hallNumber AND s.timeAndDate = :datetime")
+    Optional<Session> findByDatetimeAndHallNumber(@Param("datetime") Timestamp datetime,
+                                                  @Param("hallNumber") Integer hallNumber);
 }
