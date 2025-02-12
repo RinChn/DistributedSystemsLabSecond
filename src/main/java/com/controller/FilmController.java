e7eb631ea1c09f213bd024fbc69e3dc7bf1ea607
RinChn <morik.arina@yandex.ru>
2025-02-13 02:44:25 +0300

    Adding GET, POST and DELETE endpoints in film controller

--- src/main/java/com/controller/FilmController.java
+++ src/main/java/com/controller/FilmController.java
@@ -1,13 +1,36 @@
 package com.controller;
 
+import com.dto.FilmDto;
+import com.dto.FilmDeleteRequest;
+import com.service.FilmServiceImpl;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("api/v1/films")
 @Slf4j
 @RequiredArgsConstructor
 public class FilmController {
+    private final FilmServiceImpl filmService;
+
+    @GetMapping
+    public List<FilmDto> getAllFilms(@RequestParam(value = "page", defaultValue = "0") Integer pageNumber,
+                                     @RequestParam(value = "limit", defaultValue = "5") Integer pageSize) {
+        return filmService.getAllFilms(pageNumber, pageSize);
+    }
+
+    @PostMapping
+    public FilmDto addFilm(@Valid @RequestBody FilmDto filmDto) {
+        return filmService.addFilm(filmDto);
+    }
+
+    @DeleteMapping
+    public UUID deleteFilm(@Valid @RequestBody FilmDeleteRequest request) {
+        return filmService.deleteFilm(request);
+    }
 }
