9ae54c32e5e374a0d2755b9aac1f76f6e1619f96
RinChn <morik.arina@yandex.ru>
2025-02-13 02:41:32 +0300

    Adding service's DTO

---
+++ src/main/java/com/dto/SessionResponse.java
@@ -0,0 +1,20 @@
+package com.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.sql.Time;
+import java.util.Date;
+
+@Getter
+@Setter
+@Builder
+public class SessionResponse {
+    Date date;
+    Time time;
+    FilmDto film;
+    Integer cinemaHallNumber;
+    Integer numberOfTicketsSold;
+    Integer numberOfTicketsAvailable;
+}
---
+++ src/main/java/com/dto/SessionSearchRequest.java
@@ -0,0 +1,24 @@
+package com.dto;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+
+import java.sql.Date;
+import java.sql.Time;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class SessionSearchRequest {
+    @NotNull(message = "The time cannot be empty")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "HH:mm:ss")
+    Time time;
+    @NotNull(message = "The date cannot be empty")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
+    Date date;
+    @NotNull(message = "The room number cannot be empty")
+    Integer cinemaHallNumber;
+}
