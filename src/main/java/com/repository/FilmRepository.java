56ed1d189d0cbde7e35a1c903e20373f68c62d17
RinChn <morik.arina@yandex.ru>
2025-02-13 02:42:28 +0300

    Adding method for get film by title and director's name from film rep

--- src/main/java/com/repository/FilmRepository.java
+++ src/main/java/com/repository/FilmRepository.java
@@ -1,12 +1,18 @@
 package com.repository;
 
 
+import com.entity.Director;
 import com.entity.Film;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface FilmRepository extends JpaRepository<Film, UUID> {
+    @Query("SELECT f FROM Film f WHERE f.title = :title AND f.director = :director")
+    Optional<Film> findByTitleAndDirector(@Param("title") String title, @Param("director") Director director);
 }
